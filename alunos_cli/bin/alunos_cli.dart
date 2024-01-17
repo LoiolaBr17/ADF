824659df6124ce6d512c3b98b2460da16a0e6ba8
LoiolaBr17 <mateustaua16@gmail.com>
2024-01-17 00:40:23 -0300

    criando os models e os repositorys

--- alunos_cli/bin/alunos_cli.dart
+++ alunos_cli/bin/alunos_cli.dart
@@ -1,3 +1,35 @@
+import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
+
 void main(List<String> arguments) {
+  // final argParser = ArgParser();
+  // argParser.addFlag('data', abbr: 'd');
+  // argParser.addOption('name', abbr: 'n');
+  // argParser.addOption('template', abbr: 't');
+
+  // final argResults = argParser.parse(arguments);
+
+  // print(argResults['data']);
+
+  CommandRunner('ADF CLI','ADF CLI')
+    ..addCommand(ExemploCommand())
+    ..run(arguments);
+}
+
+class ExemploCommand extends Command {
+  @override
+  String get description => 'Exemplo de Comando';
+
+  @override
+  String get name => 'exemplo';
+
+  ExemploCommand() {
+    argParser.addOption('template', abbr: 't', help: 'Template de criação do projeto');
+  }
+
+  @override
+  void run() {
+    print('Executando o comando de exemplo');
+  }
   
 }
