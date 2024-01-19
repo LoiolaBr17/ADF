9c6faa45a87fdcfdfd75bc08f15df8262938b169
LoiolaBr17 <mateustaua16@gmail.com>
2024-01-18 23:58:55 -0300

    criando o command student find all

--- alunos_cli/bin/alunos_cli.dart
+++ alunos_cli/bin/alunos_cli.dart
@@ -1,35 +1,26 @@
-import 'package:args/args.dart';
+import 'package:alunos_cli/src/commands/students/students_command.dart';
 import 'package:args/command_runner.dart';
 
 void main(List<String> arguments) {
-  // final argParser = ArgParser();
-  // argParser.addFlag('data', abbr: 'd');
-  // argParser.addOption('name', abbr: 'n');
-  // argParser.addOption('template', abbr: 't');
-
-  // final argResults = argParser.parse(arguments);
-
-  // print(argResults['data']);
-
   CommandRunner('ADF CLI','ADF CLI')
-    ..addCommand(ExemploCommand())
+    ..addCommand(StudentsCommand())
     ..run(arguments);
 }
 
-class ExemploCommand extends Command {
-  @override
-  String get description => 'Exemplo de Comando';
+// class ExemploCommand extends Command {
+//   @override
+//   String get description => 'Exemplo de Comando';
 
-  @override
-  String get name => 'exemplo';
+//   @override
+//   String get name => 'exemplo';
 
-  ExemploCommand() {
-    argParser.addOption('template', abbr: 't', help: 'Template de criação do projeto');
-  }
+//   ExemploCommand() {
+//     argParser.addOption('template', abbr: 't', help: 'Template de criação do projeto');
+//   }
 
-  @override
-  void run() {
-    print('Executando o comando de exemplo');
-  }
+//   @override
+//   void run() {
+//     print('Executando o comando de exemplo');
+//   }
   
-}
+// }
