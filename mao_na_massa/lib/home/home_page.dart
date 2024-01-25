974ce08a883072a1605fa47ef57319c66175f407
LoiolaBr17 <mateustaua16@gmail.com>
2024-01-25 02:32:56 -0300

    testando alguns widgets

--- mao_na_massa/lib/home/home_page.dart
+++ mao_na_massa/lib/home/home_page.dart
@@ -5,8 +5,29 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Primeiro Texto'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AppBar'),
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.add_alarm_outlined))
+        ],
+      ),
+      drawer: const Drawer(
+          child: Center(
+        child: Text('Drawer Aberto'),
+      )),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          margin: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
+          color: Colors.red,
+          child: const Text('Primeiro Texto'),
+        ),
+      ),
     );
   }
 }
