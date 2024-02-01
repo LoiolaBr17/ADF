7ba1cb54b0e14a0a09f5f50bebf706ca2a785385
LoiolaBr17 <mateustaua16@gmail.com>
2024-02-01 15:45:13 -0300

    utilizando bloc pattern

---
+++ flutter_default_state_manager/lib/bloc_pattern/imc_bloc_pattern_page.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_default_state_manager/bloc_pattern/imc_bloc_pattern_controller.dart';
+import 'package:flutter_default_state_manager/bloc_pattern/imc_state.dart';
+import 'package:flutter_default_state_manager/widgets/imc_gauge.dart';
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:intl/intl.dart';
+
+class ImcBlocPatternPage extends StatefulWidget {
+  const ImcBlocPatternPage({super.key});
+
+  @override
+  State<ImcBlocPatternPage> createState() => _ImcBlocPatternPageState();
+}
+
+class _ImcBlocPatternPageState extends State<ImcBlocPatternPage> {
+  final controller = ImcBlocPatternController();
+  final pesoEC = TextEditingController();
+  final alturaEC = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  var imc = 0.0;
+
+  void _calcularIMC({required double peso, required double altura}) async {}
+
+  @override
+  void dispose() {
+    controller.dispose();
+    pesoEC.dispose();
+    alturaEC.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('IMC BLOC'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                StreamBuilder<ImcState>(
+                  stream: controller.imcOut,
+                  builder: (context, snapshot) {
+                    var imc = snapshot.data?.imc ?? 0;
+
+                    return ImcGauge(imc: imc);
+                  },
+                ),
+                const SizedBox(height: 20),
+                StreamBuilder(
+                  stream: controller.imcOut,
+                  builder: (context, snapshot) {
+                    final dataValue = snapshot.data;
+
+                    if (dataValue is ImcStateLoading) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+
+                    if (dataValue is ImcStateError) {
+                      return Text(dataValue.message);
+                    }
+
+                    return const SizedBox.shrink();
+                  },
+                ),
+                TextFormField(
+                  controller: pesoEC,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Peso'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                      locale: 'pt_BR',
+                      symbol: '',
+                      decimalDigits: 2,
+                      turnOffGrouping: true,
+                    )
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'peso obrigatório';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: alturaEC,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Altura'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                      locale: 'pt_BR',
+                      symbol: '',
+                      decimalDigits: 2,
+                      turnOffGrouping: true,
+                    )
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Altura obrigatório';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    var formValid = formKey.currentState?.validate() ?? false;
+
+                    if (!formValid) return;
+
+                    var formatter = NumberFormat.simpleCurrency(
+                      locale: 'pt_BR',
+                      decimalDigits: 2,
+                    );
+
+                    double peso = formatter.parse(pesoEC.text) as double;
+                    double altura = formatter.parse(alturaEC.text) as double;
+
+                    _calcularIMC(peso: peso, altura: altura);
+                  },
+                  child: const Text('Calcular IMC'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
