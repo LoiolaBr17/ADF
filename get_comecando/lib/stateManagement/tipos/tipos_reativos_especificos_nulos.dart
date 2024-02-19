571d9310ef3d30cf6ad73632a205b6be305bacc7
LoiolaBr17 <mateustaua16@gmail.com>
2024-02-19 16:05:41 -0300

    types no getx

---
+++ get_comecando/lib/stateManagement/tipos/tipos_reativos_especificos_nulos.dart
@@ -0,0 +1,9 @@
+
+import 'package:get/get.dart';
+
+class TiposReativosEspecificosNulos {
+  final nome = RxnString();
+  final isAluno = RxnBool();
+  final qtdCursos = RxnInt();
+  final valorCursos = RxnDouble();
+}
