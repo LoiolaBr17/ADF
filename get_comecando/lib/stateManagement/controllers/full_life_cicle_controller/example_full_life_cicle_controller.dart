0386f83e12aec95a0fa6006ad36dd2f1e2102c21
LoiolaBr17 <mateustaua16@gmail.com>
2024-02-26 17:30:16 -0300

    full_life_cicle_controller

---
+++ get_comecando/lib/stateManagement/controllers/full_life_cicle_controller/example_full_life_cicle_controller.dart
@@ -0,0 +1,48 @@
+
+import 'package:get/get.dart';
+
+class ExampleFullLifeCicleController extends FullLifeCycleController with FullLifeCycleMixin{
+  @override
+  void onInit() {
+    //  
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    // 
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    // 
+    super.onClose();
+  }
+  
+  @override
+  void onDetached() {
+    // 
+  }
+
+  @override
+  void onHidden() {
+    // 
+  }
+
+  @override
+  void onInactive() {
+    // 
+  }
+
+  @override
+  void onPaused() {
+    // 
+  }
+
+  @override
+  void onResumed() {
+    // 
+  }
+  
+}
---
+++ get_comecando/lib/stateManagement/controllers/full_life_cicle_controller/full_life_cicle_controller_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FullLifeCicleControllerPage extends StatelessWidget {
+  const FullLifeCicleControllerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page'),
+      ),
+      body: Container(),
+    );
+  }
+}
